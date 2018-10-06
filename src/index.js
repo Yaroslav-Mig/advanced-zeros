aa495bf2dd7343a5ed39735043e2bf63821f458b
Yaroslav-Mig <migyaroslav@gmail.com>
2018-10-06 19:25:43 +0300

    upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  let i = 2;
+  let multiplier = [];
+
+  while (base > 1) {
+    while (base % i === 0) {
+      multiplier.push(i);
+      base = base / i;
+    }
+    i++;
+  }
+
+  let sum = 0;
+  let power = 1;
+  const length = multiplier.length;
+  let result = [];
+
+  for (let j = 0; j < length; j++) {
+    multiplier[j] === multiplier[j + 1] ? power++ : power = 1
+
+    for (let k = multiplier[j]; k <= number; k *= multiplier[j]) {
+      sum += Math.floor(number / k);
+    }
+    result.push(Math.floor(sum / power));
+    sum = 0;
+  }
+  return Math.min.apply(null, result);
 }
